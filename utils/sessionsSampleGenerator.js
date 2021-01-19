a213156480215ddcdb6d803cf03fa806dc5d550e
antsPls <zzzhafir75@gmail.com>
2021-01-19 17:07:50 +0100

    Change variables to read from user input

--- utils/sessionsSampleGenerator.js
+++ utils/sessionsSampleGenerator.js
@@ -1,22 +1,44 @@
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
 const dayjs = require('dayjs');
 const { sessionsTable } = require('../controllers/dbController');
 const { getWeekYearAndDay } = require('./dateUtils');
 
-const userId = 3;
-const noOfWeeks = 5;
-let startDate = dayjs("2021-02-01");
+try {
+    readline.question('User ID : ', (idStr) => {
+        const userId = Number.parseInt(idStr);
 
-for (let i = 0; i < noOfWeeks * 7; i++) {
-    const dateStr = startDate.format('YYYY-MM-DD');
-    const [weekYear, dayNumber] = getWeekYearAndDay(dateStr);
+        readline.question('Number of weeks : ', (numOfWeeksStr) => {
+            const numOfWeeks = Number.parseInt(numOfWeeksStr);
 
-    sessionsTable.getOrAddSession(userId, weekYear, dayNumber);
+            readline.question('Start date (YYYY-MM-DD) : ', (startDateStr) => {
+                let startDate = dayjs(startDateStr);
 
-    const activeDuration = Math.floor(Math.random() * 8.0 * 3600);
-    const inactiveDuration = Math.floor(Math.random() * 4.0 * 3600);
+                for (let i = 0; i < numOfWeeks * 7; i++) {
+                    const dateStr = startDate.format('YYYY-MM-DD');
+                    const [weekYear, dayNumber] = getWeekYearAndDay(dateStr);
+                
+                    sessionsTable.getOrAddSession(userId, weekYear, dayNumber);
+                
+                    const activeDuration = Math.floor(Math.random() * 8.0 * 3600);
+                    const inactiveDuration = Math.floor(Math.random() * 4.0 * 3600);
+                
+                    sessionsTable.updateSession(userId, weekYear, dayNumber, 
+                        activeDuration, inactiveDuration);
+                
+                    startDate = startDate.add(1, 'day');
+                }
 
-    sessionsTable.updateSession(userId, weekYear, dayNumber, 
-        activeDuration, inactiveDuration);
-
-    startDate = startDate.add(1, 'day');
+                console.log(`Inserted/updated ${numOfWeeks * 7} session entries`);
+                readline.close();
+            });
+        });
+    });
+} catch (err) {
+    console.log("An error occurred! Aborting process...");
+    console.error(err);
+    readline.close();
 }
